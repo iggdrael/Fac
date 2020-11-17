89ee25f31f368d56a030c94607f60d1b48a5f3c1
iggdrael <42035783+iggdrael@users.noreply.github.com>
2020-11-17 17:56:01 +0100

    Update benchmark.c

--- benchmark.c
+++ benchmark.c
@@ -12,34 +12,40 @@
 #define ENTRANT 0
 #define SORTANT 1
 
-#define K 2
-#define C "ls"
-#define N 2
+int K;
+int N;
+char C[50];
 
 int main(int ac, char *av[]){
   int i;
-  
- int pid[2];
+  int pid[2];
 
- 
-
-for (int i = 0 ; i < N ; i++) {
-  switch(pid[i] = fork()) {
-    case -1:
-      printf("Erreur fork\n");
-      exit(-1);
-    case 0:
-      printf("Coucou\n");
-      break;
-    default:
-      break;
+  if ( ac != 3 ){
+    fprintf(stderr, "usage : %s <K> <C> <N>\n", av[0])  
+    exit(−1);
   }
-}
-int cr;
+  sscanf(av[1], "%d", &K);
+  sscanf(av[2], "%s", C);
+  sscanf(av[3], "%d", &N);
 
-while (wait(&cr) != -1) 
-  if (cr != 0) 
-    for (i = 0 ; i < N ; i++)
-      kill(pid[i], SIGQUIT);
+  for (int i = 0 ; i < N ; i++) {
+    switch(pid[i] = fork()) {
+      case -1:
+        printf("Erreur fork\n");
+        exit(-1);
+      case 0:
+        printf("Coucou\n");
+        break;
+      default:
+        break;
+    }
+  }
+
+  int cr;
+
+  while (wait(&cr) != -1) 
+    if (cr != 0) 
+      for (i = 0 ; i < N ; i++)
+        kill(pid[i], SIGQUIT);
 
 }
