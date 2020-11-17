569116329221d546535852a03befb4c6a963818e
iggdrael <42035783+iggdrael@users.noreply.github.com>
2020-11-17 22:35:47 +0100

    Update benchmark.c

--- benchmark.c
+++ benchmark.c
@@ -21,10 +21,10 @@ int main(int ac, char *av[]){
  struct timeval t1, t2;
  
  printf("1: %d\n", gettimeofday(&t1,NULL));
- printf("1: %d\n", t1.tv_usec);
+ printf("1: %ld\n", t1.tv_usec);
  execlp("ls", "ls", NULL);
  printf("2: %d\n", gettimeofday(&t1,NULL));
- printf("2: %d\n", t2.tv_usec);
+ printf("2: %ld\n", t2.tv_usec);
 
 
  //execlp("ls", "ls", NULL);
