5473ea835064123ac8e5aa86495dfe409a7064b0
iggdrael <42035783+iggdrael@users.noreply.github.com>
2020-11-17 22:45:57 +0100

    Update benchmark.c

--- benchmark.c
+++ benchmark.c
@@ -23,7 +23,7 @@ int main(int ac, char *av[]){
  printf("1: %d\n", gettimeofday(&t1,NULL));
  printf("1: %ld\n", t1.tv_usec);
  
- execv("ls", "ls", NULL);
+ execl("ls", "ls", NULL);
  
  printf("\n\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n\n");
  
