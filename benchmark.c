bd2633c7c00b66119e938d067b78f6717318ddca
iggdrael <42035783+iggdrael@users.noreply.github.com>
2020-11-17 17:34:09 +0100

    Update benchmark.c

--- benchmark.c
+++ benchmark.c
@@ -1,7 +1,21 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+#define ECHEC -1
+#define SUCCES 0
+#define STOP  -1
+#define ENTRANT 0
+#define SORTANT 1
 
-int main(){
+int main(int ac, char *av[]){
 
-
+  printf("test\n");
+  system("ls");
+  printf("fin\n");
 
 }
