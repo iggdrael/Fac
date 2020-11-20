33d3afb7262230ed3983253e05f8a5c7eba81c82
iggdrael <iggdrael@gmail.com>
2020-11-20 14:10:06 +0100

    ou

--- Tp4 .c
+++ Tp4 .c
@@ -12,7 +12,8 @@ void Tp4TESTER(int iTest){
 	//teste le présent module
 	Appel0(sC2("Tp4TESTER,test n°",sEnt(iTest)));
 		switch (iTest) {
-			case 11:	//L11=b;break
+			case 11:	bCfgLineaireAhDroite("S->aT; T->1", 1);
+						CfgReconnaitre("S->aT; T->1", 1);//L11=b;break
 			case 12:	//L12=mots de 2 lettres sauf le mot bb;
 			case 13:	//L13=mots qui n'ont pas de a
 			case 14:	//L14=mots de 3 lettres ou plus
@@ -20,7 +21,7 @@ void Tp4TESTER(int iTest){
 			case 21:	//L21=b
 			case 22:	//L22=b+(a+b)(a+b)
 			case 23:	//L23=a(a+).b
-			case 24:	//L24=(a+).(b^n).(a+b)*), n>0;
+			case 24:	//L24=(a+).(b^n).(a+b)*, n>0;
 			//Question 3: comme sur le sujet du TP4
 																	break;
 
